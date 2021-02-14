b6b867b4772b08eeea1cf971b32c65a1b78e638e
yuxin zhao <yuxin_zhao@berkeley.edu>
2021-02-13 23:47:43 -0800

    This is a commit

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,6 +2,7 @@ import com.leapcode.service.SpeakerService;
 import com.leapcode.service.SpeakerServiceImpl;
 
 //first testing by Adalia
+//second test
 public class Application {
     public static void main(String[] args){
         SpeakerService service = new SpeakerServiceImpl();
